afbb91c4db1bdbfe0651daa11b1f4dac1944cb3e
Jason Thai <jasonthai@Jasons-MacBook-Pro.local>
2016-04-03 16:51:32 -0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+	public static void main(String[] args) {
+		MyStack intStack = new MyStack();
+		intStack.push(50);
+		System.out.println(intStack);
+	}
+}
---
+++ src/MyStack.java
@@ -0,0 +1,39 @@
+
+public class MyStack {
+	
+	private Node myFirst;
+	
+	public MyStack() {
+		
+	}
+	
+	private class Node {
+		int item;
+		Node next;
+	}
+	
+	public boolean isEmpty() {
+		return false;
+	}
+	
+	public void push(int item) {
+		
+	}
+	
+	public int pop() {
+		return 0;
+	}
+	
+	public int peek() {
+		return 0;
+	}
+	
+	public int size() {
+		return 0;
+	}
+	
+	public String toString() {
+		String format = "[]";
+		return format;
+	}
+}
